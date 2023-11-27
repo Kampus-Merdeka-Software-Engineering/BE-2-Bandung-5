dc5b5beb21c45c842012dbfc06ead5b6a75a26d3
Gigih Ragasajiwo <gigihrs10@gmail.com>
2023-11-27 23:34:52 +0700

    mulai dari 0 ya mas

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const sequelize = require('./config/sequelize');
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const userRoutes = require('./routes/users.js');
-const app = express();
-const middlewareLogRequest = require('./middleware/log.js');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use(middlewareLogRequest);
-app.use('/users', userRoutes);
-
-app.listen(4000, () => {
-    console.log('Server running on 4000');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
--- middleware/log.js
+++
@@ -1,5 +0,0 @@
-const logRequest = (req, res, next) => {
-    console.log('Log Requsest on', req.path);
-    next();
-}
-module.exports = logRequest;
