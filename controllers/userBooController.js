8e46f9cc24f76839c26e8cb2f40ddf0231e5e39a
Bagus Kurnia <bagus.kurnia@sirclo.com>
2023-12-02 16:52:30 +0700

    feat: add postUser controller & remove unecassary overlapping config

--- controllers/userBooController.js
+++ controllers/userBooController.js
@@ -4,10 +4,27 @@ const db = require('../models')
 const Lunggo_asset = db.lunggo_asset
 const User_booking = db.user_booking
 
-const getUser= async (req,res) => {
+const getUser = async (req, res) => {
     let user_booking = await User_booking.findAll({
-        attributes:['name','email','phone','address','addres_office','date','people','price_total'],
-        where:{}
+        attributes: ['name', 'email', 'phone', 'address', 'addres_office', 'date', 'people', 'price_total'],
+        where: {}
     })
     res.send(200).send(user_booking)
+}
+
+const postUser = async (req, res) => {
+    const { name, email } = req.body;
+    try {
+        await User_booking.create({
+            name,
+            email
+        });
+        res.send(200).json({
+            success: true
+        })
+    } catch (error) {
+        res.send(500).json({
+            success: false
+        })
+    }
 }
--- models/index.js
+++ models/index.js
@@ -1,24 +1,15 @@
 const dbConfig = require('../config/config.js');
-const {Sequelize, DataTypes} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/config.js');
 
-const sequelize = new Sequelize (
-    dbConfig.DB_NAME,
-    dbConfig.DB_USER,
-    dbConfig.DB_PASSWORD, {
-        host: DB_HOST,
-        dialect: dbConfig.dialect,
-        operatorsAliases: false 
-    }
-
-)
-sequelize.authenticate()
-.then(()=>{
-    console.log('/connected')
-}
-)
-.catch(err => {
-    console.log('error');
-})
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('/connected')
+//     }
+//     )
+//     .catch(err => {
+//         console.log('error');
+//     })
 const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
@@ -26,8 +17,8 @@ db.sequelize = sequelize
 db.lunggo_asset = require('./assetModels.js')(sequelize, DataTypes)
 db.user_booking = require('./userModels.js')(sequelize, DataTypes)
 
-db.sequelize.sync({force: false})
-.then(() => {
-    console.log('re-sync')
-})
+db.sequelize.sync({ force: false })
+    .then(() => {
+        console.log('re-sync')
+    })
 module.exports = db
