02e75963c9e32865b00d780c0d6bd0a42779a801
Gigih Ragasajiwo <91274213+Ragasayn@users.noreply.github.com>
2023-12-05 15:55:15 +0700

    Delete routes directory

--- routes/assetRoutes.js
+++
@@ -1,7 +0,0 @@
-const assetController = require('../controllers/assetController')
-const router = require('express').Router()
-
-router.post('/add_lunggo_asset', assetController.add_lunggo_asset)
-router.get('/getAllAsset', assetController.getAllAsset, assetController.getSingleAsset)
-
-module.exports = router;
--- routes/userBookRoutes.js
+++
@@ -1,7 +0,0 @@
-const userController = require('../controllers/userBooController')
-const router = require('express').Router()
-
-router.get('/getUser', userController.getUser)
-router.post('/postUser', userController.postUser)
-
-module.exports = router;
