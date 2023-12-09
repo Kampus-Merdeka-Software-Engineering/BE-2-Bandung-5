09cb19273d6d5fff0edd12d43c26ddcb3ed317db
Gigih Ragasajiwo <gigihrs10@gmail.com>
2023-12-10 05:07:17 +0700

    fix cors?

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({
-  origin: 'http://127.0.0.1:5501'
-}));
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
