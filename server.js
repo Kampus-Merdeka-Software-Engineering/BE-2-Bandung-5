a48ddea4da4cb3b8685eac27a3738b422503b576
Gigih Ragasajiwo <gigihrs10@gmail.com>
2023-12-09 23:55:58 +0700

    add 0.0.0.0

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use(assetRoutes);
 app.use(userRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Listening on port ${PORT}`);
 });
